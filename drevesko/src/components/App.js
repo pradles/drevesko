9a46f7f3447cb28f962368809483f3e131ebeb0a
NejcBrnelic <96769900+NejcBrnelic@users.noreply.github.com>
2023-05-05 15:55:15 +0200

    Update App.js

--- drevesko/src/components/App.js
+++ drevesko/src/components/App.js
@@ -11,6 +11,39 @@ const ListContainer = styled.div`
   flex-direction: row;
 `
 
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #555;
+  padding: 10px;
+  border: 1px solid #444;
+  border-radius: 5px;
+  margin-bottom: 10px;
+`
+const HomeButton = styled.button`
+  background-color: lightgray;
+  border: none;
+  padding: 10px;
+  margin-right: 5px;
+  transition: background-color 0.2s ease-in-out;
+
+  &:hover {
+    background-color: darkgray;
+  }
+`;
+
+const MenuButton = styled.button`
+  background-color: lightgray;
+  border: none;
+  padding: 10px;
+  transition: background-color 0.2s ease-in-out;
+
+  &:hover {
+    background-color: darkgray;
+  }
+`;
+
 class App extends Component {
   
   onDragEnd = (result) =>{
@@ -28,44 +61,39 @@ class App extends Component {
       draggableId,
       type
     ))
-
-
   }
   
   render() {
-
     const { lists } = this.props;
     return (
       <DragDropContext onDragEnd={this.onDragEnd}>
-      <div className="App">
-        <h1>Hello World</h1>
-        <Droppable droppableId='full-site' direction="horizontal" type="list" >
-          {provided =>(
-            <ListContainer {...provided.droppableProps} ref={provided.innerRef}>
-              { lists.map((list, index) => 
-              <DreveskoList listId={list.id} key={list.id} title={list.title} cards={list.cards} index={index} />
-              )}
-              {provided.placeholder}
-              <DreveskoActionButton list />
-
-            </ListContainer>
-          )}
-        </Droppable>
-      </div>
+        <div className="App">
+          <Header>
+            <div>
+              <HomeButton>Home</HomeButton>
+              <MenuButton>Menu</MenuButton>
+            </div>
+            <div style={{color: 'white'}}>Username</div>
+          </Header>
+          <Droppable droppableId='full-site' direction="horizontal" type="list" >
+            {provided =>(
+              <ListContainer {...provided.droppableProps} ref={provided.innerRef}>
+                { lists.map((list, index) => 
+                  <DreveskoList listId={list.id} key={list.id} title={list.title} cards={list.cards} index={index} />
+                )}
+                {provided.placeholder}
+                <DreveskoActionButton list />
+              </ListContainer>
+            )}
+          </Droppable>
+        </div>
       </DragDropContext>
     );
   }
 }
-/*
-const styles = {
-  listContainer: {
-    display: "flex",
-    flexDirection: "row",
-  }
-}
-*/
+
 const mapStateToProps = state =>({
   lists: state.lists
 })
 
-export default connect(mapStateToProps) (App);
+export default connect(mapStateToProps)(App);
